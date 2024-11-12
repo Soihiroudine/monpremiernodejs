15499c27e7095e67f6ce83160ba972deaa551cbf
Soihiroudine <59316622+Soihiroudine@users.noreply.github.com>
2024-11-12 16:06:35 +0300

    ajout de commentaire dans server.js

--- server.js
+++ server.js
@@ -13,3 +13,18 @@ server.listen(3000, () => {
     console.log('Listening on localhost:3000');
 });
 
+
+/* 
+ ==== En résumé ====
+1. Le projet Node est initialisé avec la commande " npm init
+
+2. un serveur Node basique est lancé grâce
+à la méthode createServer venant du package http "http.createServer()"
+
+Les paramètres 'req' et 'res' sont obligatoires pour createServer().
+ - 'req' signifie la requête de l'utilisateur effectuée le navigateur web
+ - 'res' signifie la réponse retournée par le serveur
+
+Dans notre serveur ici il retourne le message : "Bonjour le serveur est lancé !"
+grace à res.end()
+*/
